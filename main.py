e7ef8296ddd3c25aa75dae72a8272b82804728f8
Kunal Chauhan <Kunal.Chauhan@bain.com>
2023-05-19 14:03:08 +0530

    Added text OCR

---
+++ main.py
@@ -0,0 +1,101 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+
+import sys
+import time
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+'''
+Authenticate
+Authenticates your credentials and creates a client.
+'''
+subscription_key = os.getenv('subscription_key')
+endpoint = os.getenv('endpoint')
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+'''
+END - Authenticate
+'''
+
+'''
+Quickstart variables
+These variables are shared by several examples
+'''
+# Images used for the examples: Describe an image, Categorize an image, Tag an image, 
+# Detect faces, Detect adult or racy content, Detect the color scheme, 
+# Detect domain-specific content, Detect image types, Detect objects
+images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "images")
+remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
+'''
+END - Quickstart variables
+'''
+
+
+'''
+Tag an Image - remote
+This example returns a tag (key word) for each thing in the image.
+'''
+print("===== Tag an image - remote =====")
+# Call API with remote image
+tags_result_remote = computervision_client.tag_image(remote_image_url )
+
+# Print results with confidence score
+print("Tags in the remote image: ")
+if (len(tags_result_remote.tags) == 0):
+    print("No tags detected.")
+else:
+    for tag in tags_result_remote.tags:
+        print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+print()
+'''
+END - Tag an Image - remote
+'''
+
+'''
+OCR: Read File using the Read API, extract text - remote
+This example will extract text in an image, then print results, line by line.
+This API call can also extract handwriting style text (not shown).
+'''
+print("===== Read File - remote =====")
+# Get an image with text
+read_image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg"
+
+# Call API with URL and raw response (allows you to get the operation location)
+read_response = computervision_client.read(read_image_url,  raw=True)
+
+# Get the operation location (URL with an ID at the end) from the response
+read_operation_location = read_response.headers["Operation-Location"]
+# Grab the ID from the URL
+operation_id = read_operation_location.split("/")[-1]
+
+# Call the "GET" API and wait for it to retrieve the results 
+while True:
+    read_result = computervision_client.get_read_result(operation_id)
+    if read_result.status not in ['notStarted', 'running']:
+        break
+    time.sleep(1)
+
+# Print the detected text, line by line
+if read_result.status == OperationStatusCodes.succeeded:
+    for text_result in read_result.analyze_result.read_results:
+        for line in text_result.lines:
+            print(line.text)
+            # print(line.bounding_box)
+print()
+'''
+END - Read File - remote
+'''
+
+print("End of Computer Vision quickstart.")
+
+
+
